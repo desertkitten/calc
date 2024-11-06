a399ec1b9126cc0098d2f4f7dafd0a87bc679662
Cathy Whitteker <56772806+desertkitten@users.noreply.github.com>
2024-11-06 18:35:26 +0000

    Have variables in script

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let firstNumber = '';
 let secondNumber = '';
 let currentOperator = null;
 let shouldResetDisplay = false;
+
+const display = document.getElementById('display');
+
+// Basic math functions
