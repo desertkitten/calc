8edd769531639034804d7ee980bab87497779e0d
Cathy Whitteker <56772806+desertkitten@users.noreply.github.com>
2024-11-06 18:55:58 +0000

    Can't divide by 0.

--- script.js
+++ script.js
@@ -20,5 +20,6 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) return "Imagine that you have zero cookies and you split them evenly among zero friends. How many cookies does each person get? See? It doesn’t make sense. And Cookie Monster is sad that there are no cookies, and you are sad that you have no friends.";
     return a / b;
 }
