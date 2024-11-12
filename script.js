aec745228fcb09f9cd5ae224836d45188cb9cfef
Cathy Whitteker <56772806+desertkitten@users.noreply.github.com>
2024-11-12 16:54:13 +0000

    function operate coded

--- script.js
+++ script.js
@@ -69,3 +69,18 @@ function calculate() {
     );
     currentOperator = null;
 }
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return null;
+    }
+}
